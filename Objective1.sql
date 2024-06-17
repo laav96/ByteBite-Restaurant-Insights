3ce3a29e997cf9b690ad18fd0c5ea8103706c6b0
Akeyla Virgin <93278622+laav96@users.noreply.github.com>
2024-06-16 21:09:57 -0400

    Update and rename Objective 1 to Objective1.sql

--- Objective1.sql
+++ Objective1.sql
@@ -1,6 +1,6 @@
-OBJECTIVE 1
-Explore the items table
-Your first objective is to better understand the items table by finding the number of rows in the table, the least and most expensive items, and the item prices within each category.
+-- OBJECTIVE 1
+-- Explore the items table
+-- Your first objective is to better understand the items table by finding the number of rows in the table, the least and most expensive items, and the item prices within each category.
 
 -- 1. View the menu_items table
 SELECT * FROM menu_items;
