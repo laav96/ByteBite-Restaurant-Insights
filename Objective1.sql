b8a60dd0c9c7ec4aadc484921556d15361548fa7
Akeyla Virgin <93278622+laav96@users.noreply.github.com>
2024-06-26 17:22:59 -0400

    Update Objective1.sql

--- Objective1.sql
+++ Objective1.sql
@@ -8,16 +8,24 @@ SELECT * FROM menu_items;
 -- 2. Find the number of items on the menu
 SELECT COUNT(*) FROM menu_items;
 
+-- # RESULT: 32 items
+
 -- 3.What are the least and most expensive items on the menu?
 SELECT * FROM menu_items
 ORDER BY price;
 
+-- # RESULT: Edamame - Asian
+
 SELECT * FROM menu_items
 ORDER BY price DESC;
+
+-- # RESULT: Shrimp Scampi - Italian
 
 -- 4. How many Italian dishes are on the menu?
 SELECT count(*) FROM menu_items
 WHERE category = 'Italian';
+
+-- # RESULT: 9 Italian Dishes
 
 -- 5. What are the least and most expensive Italian dishes on the menu?
 SELECT *
@@ -25,17 +33,25 @@ FROM menu_items
 WHERE category = 'Italian'
 ORDER BY price;
 
+-- # RESULT: Least Expensive: Spaghetti $14.50
+
 SELECT * 
 FROM menu_items
 WHERE category = 'Italian'
 ORDER BY price DESC;
+
+-- # RESULT: Most Expensive: Shrimp Scampi $19.95
 
 -- 6. How many dishes are in each category?
 SELECT category, COUNT(menu_item_id) AS num_dishes
 FROM menu_items
 GROUP BY category;
 
+-- # RESULT: American (6), Asian (8), Mexican (9), Italian (9)
+
 -- 7. What is the average dish price within each category?
-SELECT category, AVG(price) AS avg_price
+SELECT category, ROUND(AVG(price),2) AS avg_price
 FROM menu_items
 GROUP BY category;
+
+-- # RESULT: American ($10.07), Asian ($13.48), Mexican ($11.80), Italian ($16.75)
