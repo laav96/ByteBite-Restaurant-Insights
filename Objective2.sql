81bc7bbe54d0c76975029ad73168bb2e9c8c1e98
Akeyla Virgin <93278622+laav96@users.noreply.github.com>
2024-06-26 17:27:18 -0400

    Update Objective2.sql

--- Objective2.sql
+++ Objective2.sql
@@ -10,17 +10,25 @@ SELECT * FROM order_details;
 -- What is the date range of the table?
 SELECT MIN(order_date), MAX(order_date) FROM order_details;
 
+-- RESULT: 2023-01-01 ~ 2023-03-31
+
 -- How many orders were made within this date range?
 SELECT COUNT(DISTINCT order_id) FROM order_details;
 
+-- RESULT: 5,370 Orders
+
 -- How many items were ordered within this date range?
 SELECT COUNT(*) FROM order_details;
+
+-- RESULT: 12,234 items
 
 -- Which orders had the most number of items?
 SELECT order_id, COUNT(item_id) AS num_items
 FROM order_details
 GROUP BY order_id
 ORDER BY num_items DESC;
+
+-- RESULT: Order ID: 330, 440, 443, 1957, 2657, 3473, 4305
 
 -- How many orders had more than 12 items?
 SELECT COUNT(*) FROM 
@@ -30,3 +38,4 @@ FROM order_details
 GROUP BY order_id
 HAVING num_items > 12) AS num_orders;
 
+-- RESULT: 20 orders 
